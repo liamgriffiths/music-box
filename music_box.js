2c6234daba3fd59d5c80e93b63a2050661688dae
Liam Griffiths <liamgriffiths@gmail.com>
2013-10-30 12:31:30 -0400

    remove cljs not in use

--- music_box.js
+++
@@ -1,3 +0,0 @@
-goog.addDependency("base.js", ['goog'], []);
-goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.array', 'goog.array', 'goog.object', 'goog.object', 'goog.string.StringBuffer', 'goog.string.StringBuffer', 'goog.string', 'goog.string']);
-goog.addDependency("../music-box/core.js", ['music_box.core'], ['cljs.core']);
