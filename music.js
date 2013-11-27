de264cab6f12c4b69a38fe4c434005f1f481f5f0
Liam Griffiths <liamgriffiths@gmail.com>
2013-11-27 18:11:56 -0500

    refactoring, trying to tie down global vars

--- music.js
+++ music.js
@@ -7,181 +7,201 @@
 // https://developer.tizen.org/documentation/articles/advanced-web-audio-api-usage
 // http://joshondesign.com/p/books/canvasdeepdive/chapter12.html
 
-var canvas = document.getElementById('canvas'),
-    context = canvas.getContext('2d'),
-    audioContext = new (window.AudioContext || window.webkitAudioContext)(),
-    tempo = 60,
-    tempoControl = document.getElementById('tempo'),
-    gain = 0.5,
-    gainControl = document.getElementById('gain'),
-    size = 40,
-    rows = 12,
-    cols = 32,
-    startFreq = 440 * 2, // one octacve between 440 && 880
-    paused = false, // whether we are currently moving the canvas
-    now = audioContext.currentTime,
-    stringPosition,
-    sound;
+window.requestAnimFrame = (function(){
+  return window.requestAnimationFrame  ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame    ||
+    function(callback) { window.setTimeout(callback, 1000 / 60); };
+})();
 
-var squares = [];
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
-window.onload = function() {
-  function setup() {
-    canvas.width = screen.width;
-    canvas.height = size * rows + (size / 2.5) * rows + size;
-    stringPosition = canvas.width / 4;
-    canvas.addEventListener('click', click, false);
-    window.addEventListener('keydown', keydown);
+function MusicBox(settings) {
+  this.rows = settings.rows || 12;
+  this.cols = settings.cols || 32;
+  this.squareSize = settings.squareSize || 40;
+  this.canvas = document.getElementById('canvas');
+  this.canvas.width = document.documentElement.clientWidth;
+  this.canvas.height = this.squareSize * this.rows + (this.squareSize / 2.5) * this.rows + this.squareSize;
+  this.context = this.canvas.getContext('2d');
+  this.stringPosition = settings.stringPosition || (this.canvas.width / 4);
+  this.squares = [];
 
-    for(var x = 0; x < cols; x++) {
-      for(var y = 0; y < rows; y++) {
-        squares.push(new Square(x, y));
-      }
-    }
+  this.tempo = 60;
+  this.tempoControl = document.getElementById('tempo');
+  this.gain = 1;
+  this.gainControl = document.getElementById('gain');
 
-  }
+  this.startSpeed = settings.startSpeed || (this.tempo / 60 / this.cols);
 
-  function update() {
-    canvas.width = canvas.width;
-    var len = squares.length;
-    for(var i = 0; i < len; i++){
-      var square = squares[i];
-      square.update();
-    }
+  this.audioContext = new window.AudioContext();
+  this.now = this.audioContext.currentTime;
 
-    now = audioContext.currentTime;
-    handleForm();
-  }
+  this.isPlaying = true;
 
-  function draw() {
-    // draw squares, play sounds
-    var len = squares.length;
-    for(var i = 0; i < len; i++){
-      var square = squares[i];
-      square.draw();
-      square.play();
-    }
-
-    // draw string
-    context.strokeStyle = 'white';
-    context.moveTo(stringPosition, 0);
-    context.lineTo(stringPosition, canvas.height);
-    context.stroke();
-  }
-
-  function main() {
-    update();
-    draw();
-    window.requestAnimationFrame(main);
-  }
-
-  function click(e) {
+  canvas.addEventListener('click', function(e) {
     var x = e.pageX - canvas.offsetLeft;
     var y = e.pageY - canvas.offsetTop;
+    _each(this.squares, function(square) { square.updateIfClicked(x, y); });
+  }.bind(this));
 
-    for(var i = 0; i < squares.length; i++){
-      squares[i].updateIfClicked(x, y);
-    }
-  }
-
-  function keydown(e) {
+  window.addEventListener('keydown', function(e) {
     if(e.keyCode == 32){
-      // spacebar pauses
-      paused = !paused;
+      this.isPlaying = !this.isPlaying;
       e.preventDefault();
     }
-  }
+  }.bind(this));
 
-  function handleForm() {
-    // tempo = t(tempoControl.valueyy);
-    tempo = parseInt(tempoControl.value);
-    gain = parseInt(gainControl.value);
+  for(var x = 0; x < this.cols; x++) {
+    for(var y = 0; y < this.rows; y++) {
+      var freq = 880 - ((880 / 2 / this.rows) * y);
+      this.squares.push(new Square(x, y, this.startSpeed, freq, this.cols - 1, this.squareSize));
+    }
   }
+}
 
-  setup();
-  main();
+MusicBox.prototype = {
+  update: function() {
+    // update squares
+    _each(this.squares, function(square) {
+      square.update({
+        stringPosition: this.stringPosition,
+        speed: this.isPlaying ? (this.tempo / 60) / this.cols : 0
+      });
+    }.bind(this));
+
+    this.now = this.audioContext.currentTime;
+
+    // update values from form
+    this.tempo = parseInt(this.tempoControl.value, 10);
+    this.gain = parseInt(this.gainControl.value, 10);
+  },
+
+  draw: function() {
+    // clear canvas
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    // draw squares
+    _each(this.squares, function(square) {
+      square.draw(this.context);
+    }.bind(this));
+
+    // draw string
+    this.context.strokeStyle = 'white';
+    this.context.moveTo(this.stringPosition, 0);
+    this.context.lineTo(this.stringPosition, this.canvas.height);
+    this.context.stroke();
+  },
+
+  play: function() {
+    // play squares
+    _each(this.squares, function(square) {
+      square.play(this.audioContext, this.now);
+    }.bind(this));
+  },
+
+  kickOutTheJams: function() {
+    this.update();
+    this.draw();
+    this.play();
+    window.requestAnimFrame(this.kickOutTheJams.bind(this));
+  }
 };
 
-function Square(x, y) {
+function _each(array, fn) {
+  var len = array.length;
+  for (var i = 0; i < len; i++) {
+    fn(array[i], i);
+  }
+}
+
+window.onload = function() {
+  new MusicBox({}).kickOutTheJams();
+};
+
+function Square(x, y, speed, freq, start, size) {
   this.x = x;
   this.y = y;
-  this.offset = Math.floor(size / 2.5);
+  this.size = size;
+  this.offset = Math.floor(this.size / 2.5);
   this.px = this.getPixelPosition(x);
   this.py = this.getPixelPosition(y);
   this.on = false;
   this.active = false;
   this.playing = false;
   this.sound = undefined;
-  this.freq = startFreq - ((startFreq / 2 / rows) * y);
+  this.freq = freq;
   this.color = new Color(y);
+  this.speed = speed;
+  this.start = start;
 }
 
 Square.prototype = {
-  currentSpeed: function() {
-    return paused ? 0 : (tempo / 60.0) / cols;
-  },
-
-  draw: function() {
+  draw: function(ctx) {
     if(this.isOnScreen()){
       if(this.on) {
         // active squares are currently touching the line
         if(this.active) {
-          context.fillStyle = this.color.rgba(0.9);
-          context.fillRect(this.px, this.py, size, size);
-          context.fillStyle = "rgba(255, 255, 255, 0.7)";
-          context.fillRect(this.px - 1, this.py - 1, size + 2, size + 2);
+          ctx.fillStyle = this.color.rgba(0.9);
+          ctx.fillRect(this.px, this.py, this.size, this.size);
+          ctx.fillStyle = "rgba(255, 255, 255, 0.7)";
+          ctx.fillRect(this.px - 1, this.py - 1, this.size + 2, this.size + 2);
         }else{
-          context.fillStyle = this.color.rgba(0.9);
-          context.fillRect(this.px, this.py, size, size);
+          ctx.fillStyle = this.color.rgba(0.9);
+          ctx.fillRect(this.px, this.py, this.size, this.size);
         }
       }else{
-        context.fillStyle = this.color.rgba(0.2);
-        context.fillRect(this.px, this.py, size, size);
+        ctx.fillStyle = this.color.rgba(0.2);
+        ctx.fillRect(this.px, this.py, this.size, this.size);
       }
     }
   },
 
-  update: function() {
-    this.active = this.isActive();
-    if(this.px + size < 0) this.x = cols - 1;
-    this.x -= this.currentSpeed();
+  update: function(settings) {
+    for (var prop in settings) {
+      this[prop] = settings[prop];
+    }
+
+    this.active = this.isActive(settings.stringPosition);
+    if (this.px + this.size < 0) this.x = this.start;
+    this.x -= this.speed;
     this.px = this.getPixelPosition(this.x);
     this.py = this.getPixelPosition(this.y);
   },
 
   getPixelPosition: function(p) {
-    return p * size + (p * this.offset + this.offset);
+    return p * this.size + (p * this.offset + this.offset);
   },
 
   isOnScreen: function() {
-    return (this.px < canvas.width && this.px + size > 0);
+    return (this.px < canvas.width && this.px + this.size > 0);
   },
 
-  isActive: function() {
-    return (this.px < stringPosition && this.px + size > stringPosition);
+  isActive: function(stringPosition) {
+    return (this.px < stringPosition && this.px + this.size > stringPosition);
   },
 
   updateIfClicked: function(cx, cy) {
-    if(cx > this.px && cx < this.px + size){
-      if(cy > this.py && cy < this.py + size){
+    if(cx > this.px && cx < this.px + this.size){
+      if(cy > this.py && cy < this.py + this.size){
         this.on = !this.on;
       }
     }
     return this.on;
   },
 
-  play: function() {
+  play: function(actx, time) {
     if(! this.playing && this.on && this.active){
         // create a synthetic tone for now
         // replace with microphone?
-        var tone = audioContext.createOscillator();
+        var tone = actx.createOscillator();
         tone.type = 1;
         tone.frequency.value = this.freq;
 
         // create a new sound and play it (now)
-        this.sound = new Sound(tone);
+        this.sound = new Sound(actx, tone);
         this.playing = true;
-        this.sound.start(now);
+        this.sound.start(time);
     }else if(this.playing && this.on && !this.active){
       this.sound.stop(0);
       this.playing = false;
@@ -212,10 +232,10 @@ function Color(i) {
 
 // takes a source and pushes it through available effect audio nodes to output
 // returns a context
-function Sound(source) {
+function Sound(actx, source) {
 
   // create a gain node - which can be used to alter the volume of the input
-  var volume = audioContext.createGain();
+  var volume = actx.createGain();
   volume.gain.value = gain;
   source.connect(volume);
 
@@ -224,12 +244,12 @@ function Sound(source) {
   // volume.connect(reverb);
 
   // create delay node
-  var delay = audioContext.createDelay();
+  var delay = actx.createDelay();
   delay.delayTime.value = 0;
   volume.connect(delay);
 
 
-  delay.connect(audioContext.destination);
+  delay.connect(actx.destination);
   return source;
 }
 
